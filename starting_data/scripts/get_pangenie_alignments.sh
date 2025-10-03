c00d73e79a62fbb2f7e33546cb9d3b512c34ece0
Jacob Blindenbach <jab7dq@virginia.edu>
2025-10-03 16:58:44 -0400

    fixup

--- starting_data/scripts/get_pangenie_alignments.sh
+++ starting_data/scripts/get_pangenie_alignments.sh
@@ -1,2 +1,2 @@
 wget https://zenodo.org/records/7669083/files/grch38_all-samples_bi_all.vcf.gz?download=1 -O PG.vcf.gz
-bcftools index PG.vcf.gz
+wget https://zenodo.org/records/7669083/files/grch38_all-samples_bi_all.vcf.gz.tbi?download=1 -O PG.vcf.gz.tbi
